bf486fab1d7c1c6f226232dbbd2d664a3fc3367f
DigitalDadKC <raleighgroesbeck@gmail.com>
2025-10-18 06:37:02 -0500

    added production images

--- database/seeders/production/skills.sql
+++ database/seeders/production/skills.sql
@@ -1,13 +1,13 @@
 INSERT INTO `skills` (`id`, `name`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'HTML5', 'skills/S0eYtH4DmnrrHmuyJz8TMEL4aegWxt93AuE2mEHU.svg', '2023-12-29 18:26:31', '2025-10-14 02:51:22'),
-(2, 'CSS3', 'skills/9ZUoMj1mdQ923IltiyxruWonE7X0PMZA359BbzY5.svg', '2023-12-29 18:34:56', '2025-10-14 02:51:30'),
-(3, 'JavaScript', 'skills/1CzVryz9LIIjrnrK8veqynfgEnqDLAr1ciBNoaaH.svg', '2023-12-29 18:38:27', '2025-10-14 02:51:42'),
-(4, 'jQuery', 'skills/72SZLZsgHFvZjQiggoTpAZGh93iUpgzHb7W5juUl.svg', '2023-12-29 18:42:08', '2025-10-14 02:51:52'),
-(5, 'Tailwind', 'skills/bA2px27xVoDdlFBckZz1JriENeqgH2M334BB0grq.svg', '2023-12-29 18:43:34', '2025-10-14 02:52:02'),
-(6, 'Laravel', 'skills/Yz9gJAt8JwQvOnbRBh5Jnpd3O2YCSLe111NyObKi.svg', '2023-12-29 18:46:08', '2025-10-14 02:52:09'),
-(7, 'Livewire', 'skills/Cf9xPNww6gMVl3ebaxBRhJOCpkrZS5AfBeISK0BT.svg', '2023-12-29 18:50:38', '2025-10-14 02:52:39'),
-(8, 'Vue', 'skills/DzaWqZWACvloroYXZJLISQgeymPBpmdjOy6NQTwY.svg', '2023-12-29 18:53:55', '2025-10-14 02:52:33'),
-(9, 'Bootstrap', 'skills/Tu4kqLMSPeLg3hHWzRm9gBosCOXTp3OBSbmaVuvh.svg', '2024-01-04 00:51:56', '2025-10-14 02:52:47'),
-(10, 'Excel VBA', 'skills/pxvp4oqpDAos4obArQfAvQjLWrA90yDZolmcrZmq.svg', '2024-01-04 01:35:56', '2025-10-14 19:28:23');
+(1, 'HTML5', 'skills/N6CcESsENlKj1Zc5fSnzeuW9mW6oyjq8TZGqoWWE.svg', '2023-12-29 18:26:31', '2025-10-18 11:28:00'),
+(2, 'CSS3', 'skills/aN4ft20cKVTCosTX1vW0YcmbmiuRZ9XTbgvjh2VR.svg', '2023-12-29 18:34:56', '2025-10-18 11:28:28'),
+(3, 'JavaScript', 'skills/G339xqVhjgHgrEIDSns0CQ2TG8AIvJBSvJ5TGSZf.svg', '2023-12-29 18:38:27', '2025-10-18 11:28:37'),
+(4, 'jQuery', 'skills/6xP7FHFUGfX6m4qCzWABC1hplHIrWfcSq6ZZm0aE.svg', '2023-12-29 18:42:08', '2025-10-18 11:28:44'),
+(5, 'Tailwind', 'skills/9Vc6WztmGTqQKF3MwlGoavgxTqdRsKu4m5jrqTL1.svg', '2023-12-29 18:43:34', '2025-10-18 11:28:48'),
+(6, 'Laravel', 'skills/YHpZoBp1QefADFAVDRjgi07xVtNda8wcWBSHMseV.svg', '2023-12-29 18:46:08', '2025-10-18 11:28:53'),
+(7, 'Livewire', 'skills/1oh4HSDh1cz6JHA2HbpFkpFaOxNHFjHY6nd3ch4O.svg', '2023-12-29 18:50:38', '2025-10-18 11:28:58'),
+(8, 'Vue', 'skills/ZxRo7Hksk0hQYiBv7ZnolNOF1w7lk3uPdbfzkt7l.svg', '2023-12-29 18:53:55', '2025-10-18 11:29:03'),
+(9, 'Bootstrap', 'skills/VR7Y3tXhLJyVlFeABNSbixCjxYYsMOEtwdJNYIJL.svg', '2024-01-04 00:51:56', '2025-10-18 11:29:07'),
+(10, 'Excel VBA', 'skills/MFbdwUNkNRzAbgOwQrLfnkYCcvCQf46VUYFGFbrI.svg', '2024-01-04 01:35:56', '2025-10-18 11:29:14');
 
 COMMIT;
