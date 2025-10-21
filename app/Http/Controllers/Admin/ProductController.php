1f6e7e12111c3d7615eae9210ef5ba404c260918
DigitalDadKC <raleighgroesbeck@gmail.com>
2025-10-21 07:04:14 -0500

    testing Stripe

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -44,7 +44,7 @@ class ProductController extends Controller
         }
 
         $checkout_session = \Stripe\Checkout\Session::create([
-            'line_items' => [[$lineItems]],
+            'line_items' => $lineItems,
             'mode' => 'payment',
             'success_url' => route('checkout.success') . '?session_id={CHECKOUT_SESSION_ID}',
             'cancel_url' => route('checkout.cancel', [], true),
