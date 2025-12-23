3f56c6a781ad6965d1269de1c3e7b06910ad60da
DigitalDadKC <raleighgroesbeck@gmail.com>
2025-12-23 07:04:58 -0600

    testing webhook

--- app/Http/Controllers/Admin/SignWellController.php
+++ app/Http/Controllers/Admin/SignWellController.php
@@ -2,36 +2,42 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Mail\LogMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class SignWellController extends Controller
 {
-    public function __invoke(Request $request) {
-        \Log::info('SIGNWELL WEBHOOK HIT', [
-            'headers' => $request->headers->all(),
-            'payload' => $request->all(),
-        ]);
+    // public function __invoke(Request $request) {
+    //     \Log::info('SIGNWELL WEBHOOK HIT', [
+    //         'headers' => $request->headers->all(),
+    //         'payload' => $request->all(),
+    //     ]);
 
-        return response()->json(['ok' => true]);
-    }
+    //     return response()->json(['ok' => true]);
+    // }
     public function handle(Request $request)
     {
-        Log::info('SignWell Webhook Received', $request->all());
-        $event = $request->input('event');
-        // Log everything for debugging
-
-        $event = $request->input('event');
+        Log::info('skipped');
+        Log::info('SignWell Webhook Received', $request->event);
+        $event = $request->input('event.type');
 
         match ($event) {
-            'document.completed' => $this->handleCompleted($request),
-            'document.viewed' => $this->handleViewed($request),
-            'document.sent' => $this->handleSent($request),
+            'document_created' => $this->handleCreated($request),
+            'document_completed' => $this->handleCompleted($request),
+            'document_viewed' => $this->handleViewed($request),
+            'document_sent' => $this->handleSent($request),
+            'document_canceled' => $this->handleCanceled($request),
             default => null,
         };
 
         return response()->json(['status' => 'ok']);
+    }
+
+    protected function handleCreated(Request $request) {
+        Mail::to(config('mail.from.address'))->send(new LogMail($request->all()));
     }
 
     protected function handleCompleted(Request $request)
@@ -49,5 +55,9 @@ class SignWellController extends Controller
     protected function handleSent(Request $request)
     {
         //
+    }
+
+    protected function handleCanceled(Request $request) {
+        Mail::to(config('mail.from.address'))->send(new LogMail($request->all()));
     }
 }
