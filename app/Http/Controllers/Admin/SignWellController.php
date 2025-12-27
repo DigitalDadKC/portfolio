2cf90d25cba84561edac1aec968b25478ffb3df4
DigitalDadKC <raleighgroesbeck@gmail.com>
2025-12-26 21:13:00 -0600

    google maps

--- app/Http/Controllers/Admin/SignWellController.php
+++ app/Http/Controllers/Admin/SignWellController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Mail\LogMail;
+use App\Models\Contract;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
@@ -15,48 +16,64 @@ class SignWellController extends Controller
         Log::info($request);
         $event = $request->event['type'];
         $name = $request->data['object']['recipients'][0]['name'];
+        $contract = Contract::where('signwell_id', $request->data['object']['id'])->first();
 
         match ($event) {
-            'document_created' => $this->handleCreated($name),
-            'document_sent' => $this->handleSent($name),
-            'document_viewed' => $this->handleViewed($name),
-            'document_in_progress' => $this->handleInProgress($name),
-            'document_signed' => $this->handleSigned($name),
-            'document_completed' => $this->handleCompleted($name),
-            'document_expired' => $this->handleExpired($name),
-            'document_canceled' => $this->handleCanceled($name),
-            'document_declined' => $this->handleDeclined($name),
+            'document_created' => $this->handleCreated($contract),
+            'document_sent' => $this->handleSent($contract),
+            'document_viewed' => $this->handleViewed($contract),
+            'document_in_progress' => $this->handleInProgress($contract),
+            'document_signed' => $this->handleSigned($contract),
+            'document_completed' => $this->handleCompleted($contract),
+            'document_expired' => $this->handleExpired($contract),
+            'document_canceled' => $this->handleCanceled($contract),
+            'document_declined' => $this->handleDeclined($contract),
             default => null,
         };
 
         return response()->json(['status' => 'ok']);
     }
 
-    protected function handleCreated(string $name) {
+    protected function handleCreated(Contract $contract) {
         $action = 'Created';
+        $contract->update([
+            'status' => 'created'
+        ]);
         // Mail::to(config('mail.from.address'))->send(new LogMail($name, $action));
     }
 
-    protected function handleSent(string $name)
+    protected function handleSent(Contract $contract)
     {
         $action = 'Sent';
+        $contract->update([
+            'status' => 'sent'
+        ]);
         // Mail::to(config('mail.from.address'))->send(new LogMail($name, $action));
     }
 
-    protected function handleViewed(string $name)
+    protected function handleViewed(Contract $contract)
     {
         $action = 'Viewed';
+        $contract->update([
+            'status' => 'viewed'
+        ]);
         // Mail::to(config('mail.from.address'))->send(new LogMail($name, $action));
     }
 
-    protected function handleCompleted(string $name)
+    protected function handleCompleted(Contract $contract)
     {
         $action = 'Completed';
+        $contract->update([
+            'status' => 'completed'
+        ]);
         // Mail::to(config('mail.from.address'))->send(new LogMail($name, $action));
     }
 
-    protected function handleCanceled(string $name) {
+    protected function handleCanceled(Contract $contract) {
         $action = 'Canceled';
+        $contract->update([
+            'status' => 'canceled'
+        ]);
         // Mail::to(config('mail.from.address'))->send(new LogMail($name, $action));
     }
 }
