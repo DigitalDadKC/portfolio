ebc244d6fc3502a8dfb1389dfa1921eeb5bf4224
raleigh <raleighgroesbeck@gmail.com>
2026-06-12 06:49:39 -0500

    adding oauth

--- app/Http/Controllers/Admin/GoogleAuthController.php
+++ app/Http/Controllers/Admin/GoogleAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\SocialAccount;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleAuthController extends Controller
@@ -12,6 +13,9 @@ class GoogleAuthController extends Controller
     }
 
     public function callback() {
+        $googleUser = Socialite::driver('github')->user();
 
+        $socialAccount = SocialAccount::where(['provider' => 'google', 'provider_id' => $googleUser->getId()])->first();
+        dd($googleUser, $socialAccount);
     }
 }
